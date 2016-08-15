0dad91acf43cd2aab0f9cf186c948ece645cbc5d
zhaozhiming <kingzzm1982@sina.com>
2016-08-15 22:47:44 +0800

    feat: user text title

--- src/containers/Main/components/Header/Header.js
+++ src/containers/Main/components/Header/Header.js
@@ -3,7 +3,6 @@ import style from './style.css';
 import React, { Component, PropTypes } from 'react';
 import classnames from 'classnames';
 import header from 'images/header.jpg';
-import title from 'images/title.png';
 
 class Header extends Component {
   static propTypes = {
@@ -24,12 +23,12 @@ class Header extends Component {
         className={classnames(...className.split(), style.header)}
       >
         <div className={style.top}>
-          <img className={style.title} alt="title" src={title} />
+          <span className={style.title}>好吃喵の私房菜</span>
           <span className={style.subtitle}>属于戛纳滨江的私房菜馆</span>
           <img className={style.img} alt="img" src={header} />
         </div>
         <span className={style.middle}>
-          本店菜品
+          ~欢迎光临~
         </span>
         <div className={style.bottom}></div>
       </div>
